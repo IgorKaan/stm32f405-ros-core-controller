e9fcb2a51c9f2e0aa99e8df4ee4601dfde7adb9b
Igor <igorkaan@yandex.ru>
2020-02-11 21:11:43 +0300

    update 11.02.20

--- usr/cpp_main.cpp
+++ usr/cpp_main.cpp
@@ -10,6 +10,7 @@
 #include "ros.h"
 #include "std_msgs/Byte.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int8.h"
 #include "std_msgs/Int16.h"
 #include "std_msgs/Int32.h"
 #include "nbt.h"
@@ -24,8 +25,10 @@ extern float gyroZ;
 extern float accelX;
 extern float accelY;
 extern float accelZ;
-extern int8_t sideData;
-extern int8_t speedData;
+extern int8_t sideDataRight;
+extern int8_t speedDataRight;
+extern int8_t sideDataLeft;
+extern int8_t speedDataLeft;
 struct ringbuffer rb;
 uint32_t a = 0;
 
@@ -37,11 +40,16 @@ geometry_msgs::Vector3 accel_msg;
 
 extern "C" void messageSide(const std_msgs::Byte& msg);
 extern "C" void messageSpeed(const std_msgs::Byte& msg);
+extern "C" void rpm_right_subCb(const std_msgs::Int8& msg);
+extern "C" void rpm_left_subCb(const std_msgs::Int8& msg);
 extern "C" void init_ROS();
+ros::Publisher str("gyro", &str_msg);
 ros::Publisher gyro("gyro", &gyro_msg);
 ros::Publisher accel("accel", &accel_msg);
 ros::Subscriber<std_msgs::Byte> sideD("side", messageSide);
 ros::Subscriber<std_msgs::Byte> speedD("speed", messageSpeed);
+ros::Subscriber<std_msgs::Int8> rpm_right_sub("rpm_right_sub", rpm_right_subCb);
+ros::Subscriber<std_msgs::Int8> rpm_left_sub("rpm_left_sub", rpm_left_subCb);
 
 static nbt_t publish_nbt;
 static nbt_t vector_nbt;
@@ -50,14 +58,46 @@ static nbt_t gyro_nbt;
 static nbt_t accel_nbt;
 static nbt_t ros_nbt;
 
+extern "C" void rpm_right_subCb(const std_msgs::Int8& msg)
+{
+	//if (msg.data )
+	if (msg.data >= 10) {
+		speedDataRight = msg.data;
+		sideDataRight = 1;
+	}
+	else if (msg.data <= -10) {
+		speedDataRight = -(msg.data);
+		sideDataRight = 0;
+	}
+	else {
+		speedDataRight = 0;
+	}
+}
+
+extern "C" void rpm_left_subCb(const std_msgs::Int8& msg)
+{
+	if (msg.data >= 10) {
+		speedDataLeft = msg.data;
+		sideDataLeft = 0;
+	}
+	else if (msg.data <= -10) {
+		speedDataLeft = -(msg.data);
+		sideDataLeft = 1;
+	}
+	else {
+		speedDataLeft = 0;
+	}
+}
+
+
 extern "C" void messageSide(const std_msgs::Byte& msg)
 {
-	sideData = msg.data;
+	//sideData = msg.data;
 }
 
 extern "C" void messageSpeed(const std_msgs::Byte& msg)
 {
-	speedData = msg.data;
+	//speedData = msg.data;
 }
 
 extern "C" void cdc_receive_put(uint8_t value)
@@ -68,21 +108,20 @@ extern "C" void cdc_receive_put(uint8_t value)
 extern "C" void init_ROS(void)
 {
 	ringbuffer_init(&rb, RxBuffer, RxBufferSize);
-
 	// Initialize ROS
 	nh.initNode();
-	//nh.advertise(point);
-	//nh.advertise(chatter);
+	nh.advertise(str);
 	nh.advertise(gyro);
 	nh.advertise(accel);
-	nh.subscribe(sideD);
-	nh.subscribe(speedD);
+//	nh.subscribe(sideD);
+//	nh.subscribe(speedD);
+	nh.subscribe(rpm_right_sub);
+	nh.subscribe(rpm_left_sub);
 
 	NBT_init(&gyro_nbt, 9);
 	NBT_init(&vector_nbt, 9);
 	NBT_init(&accel_nbt, 9);
 	NBT_init(&ros_nbt, 1);
-//	NBT_init(&LED_nbt, 100);
 }
 
 extern "C" void version_handler(void)
